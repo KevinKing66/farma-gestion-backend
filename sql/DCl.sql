7697bfd13fecd4b20b7183f651bc6ae89eef5bfa
Felipe <juan.garcia.vega@uniautonoma.edu.co>
2025-11-12 20:39:19 -0500

    update DCL contraseña byscript

--- sql/DCl.sql
+++ sql/DCl.sql
@@ -1,4 +1,4 @@
-CREATE USER 'farmagestion_user'@'localhost' IDENTIFIED BY 'password123';
+ALTER USER 'farmagestion_user'@'localhost' IDENTIFIED BY 'Fg@2025!Secure#';
 
 GRANT ALL PRIVILEGES ON farmagestion.* TO 'farmagestion_user'@'localhost';
 
