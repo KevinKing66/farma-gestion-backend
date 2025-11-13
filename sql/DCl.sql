2d5b819e07ffd82f0f9676cb6772b966e800df9c
KevinKing66 <kevinvallenato2002@gmail.com>
2025-11-12 21:00:45 -0500

    Se arregla la conexion a la db

--- sql/DCl.sql
+++ sql/DCl.sql
@@ -1,4 +1,7 @@
-ALTER USER 'farmagestion_user'@'localhost' IDENTIFIED BY 'Fg@2025!Secure#';
+ALTER USER 'farmagestion_user'@'localhost' IDENTIFIED BY 'password123';
+
+-- descomentar siguiente llinea solo si ienes problemas con la conexion
+-- ALTER USER 'farmagestion_user'@'localhost' IDENTIFIED BY 'Fg@2025!Secure#';
 
 GRANT ALL PRIVILEGES ON farmagestion.* TO 'farmagestion_user'@'localhost';
 
