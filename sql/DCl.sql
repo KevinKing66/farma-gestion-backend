de5301d15d6cf7782df05fcc787215c5d35a432d
KevinKing66 <kevinvallenato2002@gmail.com>
2025-11-12 21:11:20 -0500

    Se recupera el create user

--- sql/DCl.sql
+++ sql/DCl.sql
@@ -1,4 +1,4 @@
-ALTER USER 'farmagestion_user'@'localhost' IDENTIFIED BY 'password123';
+CREATE USER 'farmagestion_user'@'localhost' IDENTIFIED BY 'password123';
 
 -- descomentar siguiente llinea solo si ienes problemas con la conexion
 -- ALTER USER 'farmagestion_user'@'localhost' IDENTIFIED BY 'Fg@2025!Secure#';
