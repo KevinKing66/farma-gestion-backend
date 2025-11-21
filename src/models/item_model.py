77b4a0976e3af6e066117640e995ae459d49168f
KevinKing66 <Kevin>
2025-11-20 23:43:18 -0500

    Se arregla error menor que se provoca en algunos pythons con el tipado

--- src/models/item_model.py
+++ src/models/item_model.py
@@ -1,5 +1,8 @@
+from typing import Any
+
+
 class Item:
-    def __init__(self, id_item: int, id_ubicacion: int, codigo: str, descripcion: str, tipo_item: str, unidad_medida: str, stock_minimo: int, uso_frecuente: any | None = None):
+    def __init__(self, id_item: int, id_ubicacion: int, codigo: str, descripcion: str, tipo_item: str, unidad_medida: str, stock_minimo: int, uso_frecuente: Any | None = None):
         self.id_item = id_item
         self.id_ubicacion = id_ubicacion
         self.codigo = codigo
