4b594e594d89705dc5c2f95ae51041eed5ee92c1
KevinKing66 <kevinvallenato2002@gmail.com>
2025-11-25 19:47:36 -0500

    Se actualiza movimientos

--- src/service/movimiento_service.py
+++ src/service/movimiento_service.py
@@ -4,15 +4,27 @@ def get_all():
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
     cursor.execute("""
-        SELECT mv.id_movimiento, mv.tipo, mv.cantidad, mv.fecha,
-               mv.motivo, l.codigo_lote, u.usuario AS usuario,
-               ori.nombre AS origen, des.nombre AS destino
-        FROM movimientos_v2 mv
-        JOIN lotes l ON mv.id_lote = l.id_lote
-        JOIN usuarios u ON mv.id_usuario = u.id_usuario
-        LEFT JOIN ubicaciones ori ON mv.id_ubicacion_origen = ori.id_ubicacion
-        LEFT JOIN ubicaciones des ON mv.id_ubicacion_destino = des.id_ubicacion
-        ORDER BY mv.fecha DESC
+    SELECT 
+        mv.id_movimiento, 
+        mv.tipo, 
+        mv.cantidad, 
+        mv.fecha,
+        mv.motivo, 
+        l.codigo_lote, 
+        u.nombre_completo AS usuario,
+        ori.nombre AS origen, 
+        des.nombre AS destino
+    FROM movimientos mv
+    JOIN lotes l 
+        ON mv.id_lote = l.id_lote
+    JOIN usuarios u 
+        ON mv.id_usuario = u.id_usuario
+    LEFT JOIN ubicaciones ori 
+        ON mv.id_ubicacion_origen = ori.id_ubicacion
+    LEFT JOIN ubicaciones des 
+        ON mv.id_ubicacion_destino = des.id_ubicacion
+    ORDER BY mv.fecha DESC;
+
     """)
     result = cursor.fetchall()
     cursor.close()
